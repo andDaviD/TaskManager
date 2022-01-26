4fe96a9426bb7c640acd50d6daed39f36de5656a
Andrei Davidovich <tnu.david@gmail.com>
2022-01-26 22:41:37 +0300

    Add Cake Test support

---
+++ .build/builder/Tasks/UnittestsTask.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Cake.Common.IO;
+using Cake.Common.Tools.DotNet;
+using Cake.Common.Tools.DotNetCore.Test;
+using Cake.Frosting;
+
+namespace Build.Tasks;
+
+[TaskName("unittests")]
+public sealed class UnittestsTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        var testDirectory = context.RootDirectory.CombineWithFilePath("./**/*Tests.csproj").FullPath;
+        var testAssemblies = context.GetFiles(testDirectory).ToList();
+
+        foreach (var project in testAssemblies)
+        {
+            context.DotNetTest(project.FullPath, new DotNetCoreTestSettings
+            {
+                NoBuild = true,
+                Configuration = context.Configuration
+            });
+        }
+    }
+}
--- tests/TaskManager.Tracker.UnitTests/TaskValidationServiceTests.cs
+++ tests/TaskManager.Tracker.UnitTests/TaskValidationServiceTests.cs
@@ -3,114 +3,113 @@ using Moq;
 using NUnit.Framework;
 using TaskManager.Tracker.Contracts;
 
-namespace TaskManager.Tracker.UnitTests
+namespace TaskManager.Tracker.UnitTests;
+
+[TestFixture]
+public sealed class TaskValidationServiceTests
 {
-    [TestFixture]
-    internal sealed class TaskValidationServiceTests
+    private TaskValidationService _taskValidationService;
+
+    private Mock<IDateTimeProvider> _dateTimeProviderMock;
+
+    [SetUp]
+    public void SetUp()
     {
-        private TaskValidationService _taskValidationService;
+        _dateTimeProviderMock = new Mock<IDateTimeProvider>();
+        _taskValidationService = new TaskValidationService(_dateTimeProviderMock.Object);
+    }
+        
+    [Test]
+    public void Title_ShouldBe_Set()
+    {
+        var requestDto = new CreateTaskRequestDto(
+            string.Empty, 
+            "Test Description",
+            DateTime.Now.AddDays(1),
+            false);
 
-        private Mock<IDateTimeProvider> _dateTimeProviderMock;
+        var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
+            
+        Assert.Contains("The title is required.", errors);
+    }
         
-        [SetUp]
-        public void SetUp()
-        {
-            _dateTimeProviderMock = new Mock<IDateTimeProvider>();
-            _taskValidationService = new TaskValidationService(_dateTimeProviderMock.Object);
-        }
-        
-        [Test]
-        public void Title_ShouldBe_Set()
-        {
-            var requestDto = new CreateTaskRequestDto(
-                string.Empty, 
-                "Test Description",
-                DateTime.Now.AddDays(1),
-                false);
+    [Test]
+    public void Description_ShouldBe_Set()
+    {
+        var requestDto = new CreateTaskRequestDto(
+            "Test Title", 
+            string.Empty,
+            DateTime.Now.AddDays(1), 
+            false);
 
-            var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
+        var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
             
-            Assert.Contains("The title is required.", errors);
-        }
+        Assert.Contains("The description is required.", errors);
+    }
         
-        [Test]
-        public void Description_ShouldBe_Set()
-        {
-            var requestDto = new CreateTaskRequestDto(
-                "Test Title", 
-                string.Empty,
-                DateTime.Now.AddDays(1), 
-                false);
+    [Test]
+    public void Date_ShouldBe_InTheFuture()
+    {
+        _dateTimeProviderMock.Setup(provider => provider.Now).Returns(DateTime.Now);
+            
+        var requestDto = new CreateTaskRequestDto(
+            "Test Title", 
+            "Test Description",
+            DateTime.Now.AddDays(-1),
+            false);
 
-            var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
+        var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
             
-            Assert.Contains("The description is required.", errors);
-        }
+        Assert.Contains("The date should be in the future.", errors);
+    }
         
-        [Test]
-        public void Date_ShouldBe_InTheFuture()
-        {
-            _dateTimeProviderMock.Setup(provider => provider.Now).Returns(DateTime.Now);
+    [Test]
+    public void Request_ShouldBe_Initialized()
+    {
+        var errors = _taskValidationService.ValidateCreateTaskRequest(null);
             
-            var requestDto = new CreateTaskRequestDto(
-                "Test Title", 
-                "Test Description",
-                DateTime.Now.AddDays(-1),
-                false);
-
-            var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
-            
-            Assert.Contains("The date should be in the future.", errors);
-        }
+        Assert.Contains("The request must be initialized.", errors);
+    }
         
-        [Test]
-        public void Request_ShouldBe_Initialized()
-        {
-            var errors = _taskValidationService.ValidateCreateTaskRequest(null);
+    [Test]
+    public void Title_ShouldHave_ValidLength()
+    {
+        var requestDto = new CreateTaskRequestDto(
+            new string('T', 1000),
+            "Test Description",
+            DateTime.Now.AddDays(1),
+            false);
             
-            Assert.Contains("The request must be initialized.", errors);
-        }
+        var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
+            
+        Assert.Contains("The title should not exceed 250 characters.", errors);
+    }
         
-        [Test]
-        public void Title_ShouldHave_ValidLength()
-        {
-            var requestDto = new CreateTaskRequestDto(
-                new string('T', 1000),
-                "Test Description",
-                DateTime.Now.AddDays(1),
-                false);
+    [Test]
+    public void Description_ShouldHave_ValidLength()
+    {
+        var requestDto = new CreateTaskRequestDto(
+            "Test Title",
+            new string('T', 2000),
+            DateTime.Now.AddDays(1),
+            false);
             
-            var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
+        var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
             
-            Assert.Contains("The title should not exceed 250 characters.", errors);
-        }
+        Assert.Contains("The description should not exceed 1000 characters.", errors);
+    }
         
-        [Test]
-        public void Description_ShouldHave_ValidLength()
-        {
-            var requestDto = new CreateTaskRequestDto(
-                "Test Title",
-                new string('T', 2000),
-                DateTime.Now.AddDays(1),
-                false);
+    [Test]
+    public void Request_Is_Valid()
+    {
+        var requestDto = new CreateTaskRequestDto(
+            "Test Title", 
+            "Test Description",
+            DateTime.Now.AddDays(1),
+            false);
             
-            var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
+        var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
             
-            Assert.Contains("The description should not exceed 1000 characters.", errors);
-        }
-        
-        [Test]
-        public void Request_Is_Valid()
-        {
-            var requestDto = new CreateTaskRequestDto(
-                "Test Title", 
-                "Test Description",
-                DateTime.Now.AddDays(1),
-                false);
-            
-            var errors = _taskValidationService.ValidateCreateTaskRequest(requestDto);
-            
-            Assert.IsEmpty(errors);
-        }
+        Assert.IsEmpty(errors);
     }
 }
